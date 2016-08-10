ebbb2e2ea7396c9ff58be70f963b2ea44a347a0c
sateigen <shannon.teigen@gmail.com>
2016-08-10 10:05:05 -0400

    Fixed bug in search function.

--- search_db.py
+++ search_db.py
@@ -6,8 +6,9 @@ def ask_user():
     print("1. View the Artist Database")
     print("2. Add a row to the Artist Database")
     print("3. Search the Artist Database")
-    response = input("What would you like to do? Type 1, 2, or 3 ")
-    if response in ['1', '2', '3']:
+    print("4. Exit the Artist Database")
+    response = input("What would you like to do? Type 1, 2, 3, or 4 ")
+    if response in ['1', '2', '3', '4']:
         return int(response)
     else:
         print("\nThat was not a valid input. \n")
@@ -17,7 +18,7 @@ def ask_user():
 def view_database(cur):
     cur.execute("SELECT * FROM artists;")
     for row in cur:
-        print("Artist: {}, Title of Piece: {}, Year: {}, Medium: {}, Subject: {}, Collection: {}\n".format(row[1], row[2], row[3], row[4], row[5], row[6]))
+        print("\nArtist: {}, Title of Piece: {}, Year: {}, Medium: {}, Subject: {}, Collection: {}".format(row[1], row[2], row[3], row[4], row[5], row[6]))
 
 
 def add_row(conn, cur):
@@ -42,8 +43,8 @@ def search_database(cur):
                      }
     search_criteria = input("Would you like to search by [A]rtist, [Y]ear, [M]edium, [S]ubject, or [C]ollection ").lower()
     if search_criteria in response_dict:
-        search_item = input("Which {} would you like to search?\n > ".format(response_dict[search_criteria].lower()))
-        cur.execute("SELECT * FROM artists WHERE " + response_dict[search_criteria] + " = " + search_item)
+        search_item = input("\nWhich {} would you like to search?\n > ".format(response_dict[search_criteria].lower()))
+        cur.execute("SELECT * FROM artists WHERE " + response_dict[search_criteria] + " = %s", (search_item,))
         for row in cur:
             print("Artist: {}, Title of Piece: {}, Year: {}, Medium: {}, Subject: {}, Collection: {}\n".format(row[1], row[2], row[3], row[4], row[5], row[6]))
 
@@ -51,15 +52,18 @@ def search_database(cur):
 def main():
     conn = psycopg2.connect("dbname=artists user=shannon")
     cur = conn.cursor()
-    user_choice = ask_user()
-    if user_choice == 1:
-        view_database(cur)
-    if user_choice == 2:
-        add_row(conn, cur)
-    if user_choice == 3:
-        search_database(cur)
-    cur.close()
-    conn.close()
+    while True:
+        user_choice = ask_user()
+        if user_choice == 1:
+            view_database(cur)
+        elif user_choice == 2:
+            add_row(conn, cur)
+        elif user_choice == 3:
+            search_database(cur)
+        else:
+            cur.close()
+            conn.close()
+            break
 
 
 if __name__ == '__main__':
