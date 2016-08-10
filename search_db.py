533e9f08c0d2d609719924918ed57e90f4d83dae
sateigen <shannon.teigen@gmail.com>
2016-08-09 21:11:55 -0400

    Working function to nicely view each row in the database.

--- search_db.py
+++ search_db.py
@@ -1,23 +1,39 @@
+import psycopg2
+
+
 def ask_user():
     print("Please choose from the following options.")
     print("1. View the Artist Database")
     print("2. Add a row to the Artist Database")
     print("3. Search the Artist Database")
     response = input("What would you like to do? Type 1, 2, or 3 ")
-    if int(response) in [1, 2, 3]:
+    if response in ['1', '2', '3']:
         return int(response)
     else:
         print("\nThat was not a valid input. \n")
         ask_user()
 
 
-def view_database():
-        
+def view_database(cur):
+    cur.execute("SELECT * FROM artists;")
+    for row in cur:
+        print("Artist: {}, Title of Piece: {}, Year: {}, Medium: {}, Subject: {}, Collection: {}\n".format(row[1], row[2], row[3], row[4], row[5], row[6]))
+
+
+def add_row(cur):
+    pass
 
 
 def main():
-    if ask_user == 1:
-        view_database()
+    conn = psycopg2.connect("dbname=artists user=shannon")
+    cur = conn.cursor()
+    user_choice = ask_user()
+    if user_choice == 1:
+        view_database(cur)
+    if user_choice == 2:
+        add_row(cur)
+    cur.close()
+    conn.close()
 
 
 if __name__ == '__main__':
